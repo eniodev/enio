88fe4d26804610b364b9b0630d243a6f960e1231
whoisxnio <enioblvck@gmail.com>
2023-05-15 10:42:40 +0100

    fetching projects

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
-export const About = (): JSX.Element => {
+{/** Experimental */}
+
+import { format, parseISO } from 'date-fns';
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import React from 'react';
+import Layout from '../components/Layout';
+import { getAllPosts } from '../lib/api';
+import { PostType } from '../types/post';
+
+type IndexProps = {
+  posts: PostType[];
+};
+
+export const About = ({ posts }: IndexProps): JSX.Element => {
   return (
     <Layout
       customMeta={{
@@ -10,8 +24,39 @@ export const About = (): JSX.Element => {
     >
       <h1>🏗️</h1>
       <p>I&apos;ve been building...</p>
+
+      {/** Experimental */}
+      {posts.map((post) => (
+        <article key={post.slug} className="mt-12">
+          <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+            {format(parseISO(post.date), 'MMMM dd, yyyy')}
+          </p>
+          <h1 className="mb-2 text-xl">
+            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+              <a className="text-gray-900 dark:text-white dark:hover:text-blue-400">
+                {post.title}
+              </a>
+            </Link>
+          </h1>
+          <p className="mb-3">{post.description}</p>
+          <p>
+            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+              <a>Read More</a>
+            </Link>
+          </p>
+        </article>
+      ))}
+
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = getAllPosts(['date', 'description', 'slug', 'title']);
+
+  return {
+    props: { posts },
+  };
 };
 
 export default About;
